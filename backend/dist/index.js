6665c8e8c29587d93b68c27516f2857dad34fbf7
odysseus <terencematsune11@gmail.com>
2025-04-03 00:19:54 -0700

    added http and html/css/js files to frontend

--- backend/dist/index.js
+++ backend/dist/index.js
@@ -1,15 +1,13 @@
-import { createServer, IncomingMessage, ServerResponse } from "http";
+import { createServer } from "http";
 import { config } from "dotenv";
 console.log("connecting to backend...");
 config();
-
 const PORT = process.env.PORT;
-
-const server = createServer( (req: IncomingMessage, res: ServerResponse) => {
-    res.writeHead(200, {'Content-Type': 'text/plain'});
+const server = createServer((req, res) => {
+    res.writeHead(200, { 'Content-Type': 'text/plain' });
     res.end('Blog API');
 });
-
 server.listen(() => {
     console.log(`Server running at http://localhost:${PORT}/`);
 });
+//# sourceMappingURL=index.js.map
